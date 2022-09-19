cdec867e847be66c18e8c47eee78306705d1f098
Isabella Vieira <isabella.q.vieira@gmail.com>
2022-09-19 17:20:32 -0400

    books are adding

--- app/serializers/book_create_serializer.rb
+++ app/serializers/book_create_serializer.rb
@@ -1,5 +1,7 @@
 class BookCreateSerializer < ActiveModel::Serializer
   attributes :id, :title, :comment, :user_id
+  # attributes :id, :title, :price, :comment, :user_id, :book_genres, :book_tropes, :book_locations, :author_name
+
 
   has_many :authors
   def author_first_name
@@ -20,6 +22,25 @@ class BookCreateSerializer < ActiveModel::Serializer
     object.location.name
   end
 
+  # has_many :authors
+  # def author_name
+  #   object.authors
+  # end
+  
+  # has_many :genres
+  # def book_genres
+  #   object.genres
+  # end
+
+  # has_many :tropes
+  # def book_trope
+  #   object.tropes
+  # end
+  
+  # has_many :locations
+  # def book_locations
+  #   object.locations
+  # end
 
 
 end
